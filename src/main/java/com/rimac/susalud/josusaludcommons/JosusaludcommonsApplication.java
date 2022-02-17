50ce53e93f9cec6722d8cfcbe1f137a8aa778a01
Juan Diego Caceres Munares <44043720+juandiego436@users.noreply.github.com>
2022-02-17 14:41:59 -0500

    Initial commit

---
+++ src/main/java/com/rimac/susalud/josusaludcommons/JosusaludcommonsApplication.java
@@ -0,0 +1,13 @@
+package com.rimac.susalud.josusaludcommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosusaludcommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosusaludcommonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rimac/susalud/josusaludcommons/JosusaludcommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimac.susalud.josusaludcommons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosusaludcommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
